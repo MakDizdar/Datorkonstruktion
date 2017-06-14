d2b2fdd7e144a4484454485be26673436bb61c17
Emir Hadzisalihovic <bonte@lysator.liu.se>
2017-06-14 20:36:04 +0200

    Testing subroutines

--- util/tileGenerator.py
+++ util/tileGenerator.py
@@ -22,11 +22,25 @@ def createBoard():
     
     for y in range(30,35):
         board[y][4] = "x\"00000002\","
+	board[y][7] = "x\"00000002\","
+	board[y][9] = "x\"00000002\","
+	board[y][11] = "x\"00000002\","
+	board[y][14] = "x\"00000002\","
     
-    for x in range(4,8):
-        board[35][x] = "x\"00000002\","
-    board[40][40] = "x\"00000001\","
+    for x in range(4,6):
+        board[30][x] = "x\"00000002\","
+	board[32][x] = "x\"00000002\","
+    board[34][8] = "x\"00000002\","
+    for x in range(11,13):
+	board[30][x] = "x\"00000002\","
+	board[34][x] = "x\"00000002\","
+    board[32][15] = "x\"00000002\","
+    board[31][16] = "x\"00000002\","
+    board[33][16] = "x\"00000002\","
+    board[30][17] = "x\"00000002\","
+    board[34][17] = "x\"00000002\","
     return board
+	
 
 def write_board_to_file(board):
     f=open('board','w')
