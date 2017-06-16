bba38d534f121e196937953c59b6783bf7d7ad0e
Emir Hadzisalihovic <bonte@lysator.liu.se>
2017-06-16 20:35:59 +0200

    Testing game snake

--- src/cpu/proj_tb.vhd
+++ src/cpu/proj_tb.vhd
@@ -10,21 +10,38 @@ ARCHITECTURE behavior OF proj_tb IS
   --Component Declaration for the Unit Under Test (UUT)
   COMPONENT proj
   PORT(clk : IN std_logic;
-       rst : IN std_logic);
+       rst : IN std_logic;
+       key: in unsigned(7 downto 0);
+       beep_en:out std_logic;
+       tile: out unsigned(3 downto 0);
+       index: out unsigned(15 downto 0);
+       score : out unsigned(15 downto 0);
+	random :in unsigned(12 downto 0));
   END COMPONENT;
 
   --Inputs
   signal clk : std_logic:= '0';
   signal rst : std_logic:= '0';
+  signal key : unsigned(7 downto 0) := x"00";  -- fuck u
+  signal beep_en : std_logic := '0';
+  signal tile : unsigned(3 downto 0) := x"0";
+  signal index : unsigned(15 downto 0) := x"0000";
+  signal score : unsigned(15 downto 0) := x"0000";
+  signal random : unsigned(12 downto 0) := "0000000000000";
 
-  --Clock period definitions
   constant clk_period : time:= 1 us;
 
 BEGIN
   -- Instantiate the Unit Under Test (UUT)
   uut: proj PORT MAP (
     clk => clk,
-    rst => rst
+    rst => rst,
+    key => key,
+    beep_en =>beep_en,
+    tile => tile,
+    index => index,
+    score => score,
+    random =>random
   );
 		
   -- Clock process definitions
